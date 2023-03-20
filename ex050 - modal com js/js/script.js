537dce444444970bf0d54c75b85cb7ac739fc404
Mateus Alves <contatomateusalves@hotmail.com>
2023-03-20 20:37:36 -0300

    Upload - exercícios JS 🚀

--- ex050 - modal com js/js/script.js
+++ ex050 - modal com js/js/script.js
@@ -1,6 +1,6 @@
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
-const fade = document.querySelector("#fade");
+const fade = document.querySelector("#fade-modal");
 const modal = document.querySelector("#modal");
 
 const toggleModal = () => {
